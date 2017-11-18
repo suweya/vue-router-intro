b1cbecb0f0a6120fbcc2e523095569597f4fedb9
suweya <suweya89@gmail.com>
2017-11-18 15:31:18 +0800

    import normallize.css

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import 'normalize.css/normalize.css'
 
 Vue.use(ElementUI)
 
