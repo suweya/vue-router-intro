79436a4b1c5204a521e787dff39d4c46ffcb2a4e
suweya <suweya89@gmail.com>
2017-11-22 22:15:22 +0800

    build

--- docs/static/js/3.08c61e501e92dfa5e24d.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([3],{"2P3s":function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var r=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h1",[e._v("命名路由")]),e._v("\n  通过此方式访问\n  "),t("router-link",{attrs:{to:{name:"named",params:{id:789}}}},[e._v("User")])],1)},l=[],s={render:r,staticRenderFns:l},a=s,u=t("VU/8"),i=u(null,a,!1,null,null,null);n.default=i.exports}});
-//# sourceMappingURL=3.08c61e501e92dfa5e24d.js.map
---
+++ docs/static/js/4.c0fd3ec961d4fbc7bf32.js
@@ -0,0 +1,2 @@
+webpackJsonp([4],{"2P3s":function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var r=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h1",[e._v("命名路由")]),e._v("\n  通过此方式访问\n  "),t("router-link",{attrs:{to:{name:"named",params:{id:789}}}},[e._v("User")])],1)},l=[],s={render:r,staticRenderFns:l},a=s,u=t("VU/8"),i=u(null,a,!1,null,null,null);n.default=i.exports}});
+//# sourceMappingURL=4.c0fd3ec961d4fbc7bf32.js.map
